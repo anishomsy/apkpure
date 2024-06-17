09a70397e3b830c3ff460ef4c8819d1ece6c1ea0
ananito <anisfhomsi@gmail.com>
2024-06-17 09:03:42 +0300

    testing

--- usage.py
+++ usage.py
@@ -4,33 +4,26 @@ from apkpure.apkpure import ApkPure
 api = ApkPure()
 
 
-allResults = api.download('free fire')
-print(allResults)
-# top_result = api.search_top("WhaftsApp")
-# print(top_result)
+# allResults = api.download('free fire')
+# print(allResults)
+top_result = api.search_top("WhaftsApp")
+print(top_result)
 #
-# # Search for all results
+# Search for all results
 
-<<<<<<< HEAD
-#all_results = api.search_all("
-#            download_link =  result.find("a")
-#")
-#print(all_results)
-=======
-# all_results = api.search_all("WhdsatsApp")
-# print(all_results)
->>>>>>> 6956624 (testing)
+all_results = api.search_all("Tiktok")
+print(all_results)
 #
-# # Get app versions
-# versions = api.get_versions("WhatsApp")
-# print(versions)
-#
+# Get app versions
+versions = api.get_versions("WhatsApp")
+print(versions)
+
 # # Get app info
 # app_info = api.get_info("WhatsApp")
 # print(app_info)
 #
 # Download the latest version of an app
-print(api.download("free fire"))
+# print(api.download("facebook lite"))
 # print(os.path.getsize("./WhatsApp Messenger_2.24.13.9_APKPure.apk"))
 # Download a specific version of an app
 # api.download("WhatsApp", version="3.21.1.15")
