8bf5a5703ed127b969f20f2fc638d8396ff61025
Thawan Barroso <thawancomt@gmail.com>
2024-06-26 14:13:00 +0100

    Documentation on code
    Was added documentation on the code to help and improve the readability,
    Also some methods was changed its name.
    Gave an alias to the extractor module.

--- apkpure/apkpure.py
+++ apkpure/apkpure.py
@@ -7,61 +7,107 @@ import cloudscraper
 
 
 # Utils
-from . import extractors
+from . import extractors as scraper
 
 class ApkPure:
     def __init__(self) -> None:
         self.query_url = "https://apkpure.com/search?q="
 
-    def __check_name(self, name):
+    def __check_name(self, name : str) -> None :
+        """Verify if the query is valid to avoid errors
+        
+        Keyword arguments:
+        name : the query
+        Return: None | exit
+        """
+        
         name = name.strip()
+        name.replace(":", " ")
         if not name:
             sys.exit(
                 "No search query provided!",
             )
 
     def __soup_factory(self, url) -> BeautifulSoup:
+        """Returns soup object from an given URL,
+        using cloudscraper to avoid cloudflare anti-bot protection
+        
+        Keyword arguments:
+        url : the website URL
+        Return: BeautifulSoup
+        """
+        
         response = self.__get_response(url=url)
-        # Since response could be None check and exit if it is
         if not response:
-            # Exit the program with a return code of 1. Return 0 if successful
-            sys.exit("Error: Response is None!")
+            sys.exit("Error: Bad request!")
         return BeautifulSoup(response.text, "html.parser")
 
     def __get_response(self, url: str, **kwargs) -> requests.Response | None:
-        response = requests.get(url)
+        """A wrapper for requests.get to avoid cloudflare anti-bot protection
+        this function accept any argument for requests.get, like stream, timeout, etc.
+        Also, it dont need header, by default the cloudscraper already set the header.
+        
+        Keyword arguments:
+        url : the target url
+        kwargs : additional arguments for requests.get
+        example:
+        __get_response("https://google.com", timeout = 10)
+        Return: resquest.Response | None
+        """
 
+        scraper = cloudscraper.create_scraper()
+        response = scraper.get(url=url, **kwargs)
+        
         if response.status_code == 403:
-            scraper = cloudscraper.create_scraper()
-            response = scraper.get(url=url, **kwargs)
+            sys.exit("Error: Request was blocked, try to update the ApkPure API!")
 
-        # Return the response if the response is successful i.e status_code == 200
         return response if response.status_code == 200 else None
 
     
 
-    def search_top(self, name: str) -> str | Exception:
+    def get_first_app_result(self, name: str) -> str | Exception:
+        
+        """Get first app result from the search result page in ApkPure.
+        Is harder to get app not found, because the search result is dynamic.
+        To avoid the app not found, use the check_name method.
+        
+        Keyword arguments:
+        name : the query
+        Return: JSON string or Exception
+        """
         self.__check_name(name)
 
         query_url = self.query_url + name
         soup_obj = self.__soup_factory(query_url)
 
-        # The div element
         first_div: BeautifulSoup = soup_obj.find("div", class_="first")
-        # package_url for first result
+        
         package_url = first_div.find("a", class_="first-info")
 
         if first_div is None:
-            raise Exception("App not found")
+            raise LookupError("App not found")
 
         if package_url is None:
             package_url = first_div.find("a", class_="dd")
 
-        result = extractors.extract_info_from_search(first_div)
+        result = scraper.extract_info_from_search(first_div)
 
         return json.dumps(result, indent=4)
-
-    def search_all(self, name: str) -> list[dict]:
+    
+    def get_top_app(self, name):
+        """A wrapper for the get first app result method.
+        """
+        
+        return self.get_first_app_result(name)
+    
+    def get_all_apps_results(self, name: str) -> list[dict]:
+        """Get all app results from the search result page in ApkPure.
+        
+        Keyword arguments:
+        name : the query
+        Return: JSON string or Exception
+        """
+        
         self.__check_name(name)
 
         url = self.query_url + name
@@ -72,16 +118,27 @@ class ApkPure:
         list_of_apps = soup.find("ul", id="search-res")  # UL
         apps_in_list_of_apps = list_of_apps.find_all("li")  # LI's
 
-        all_results = [extractors.extract_info_from_search(first_app)]
+        all_results = [scraper.extract_info_from_search(first_app)] # add the first result from search to the all results
 
-        for app in apps_in_list_of_apps:
-            all_results.append(extractors.extract_info_from_search(app))
-            
+        all_results.extend(
+            scraper.extract_info_from_search(app)
+            for app in apps_in_list_of_apps
+        )
+        
         return json.dumps(all_results, indent=4)
 
     def get_versions(self, name) -> str:
+        """Get all versions of the query app.
+        The app is the first result from the query, so check if get_first_app_result is
+        giving you what app that you want.
+        
+        Keyword arguments:
+        name : the query
+        Return: an JSON array of versions or Exception
+        """
+        
         self.__check_name(name)
-        first_app_from_search : dict = json.loads(self.search_top(name))
+        first_app_from_search : dict = json.loads(self.get_first_app_result(name))
         
         version_url = str(first_app_from_search.get('package_url')) + "/versions"
         
@@ -94,30 +151,72 @@ class ApkPure:
         
         all_versions : list[dict]= []
         
-        for item in items:
-            data : BeautifulSoup = item.find("a", class_="ver_download_link") # tag a
+        all_versions = [
+            scraper.extract_info_from_versions(item.find("a", class_="ver_download_link" ))
+            for item in items
+        ]
+        
+        # for item in items:
+        #     data : BeautifulSoup = item.find("a", class_="ver_download_link") # tag a
             
-            all_versions.append(
-                extractors.extract_info_from_versions(data)
-            )
+        #     all_versions.append(
+        #         scraper.extract_info_from_versions(data)
+        #     )
             
         return json.dumps(all_versions, indent=4)
 
     def get_info(self, name: str) -> dict:
+        """Get all information about the query app.
+        The app is the first result from the query, so check if get_first_app_result is
+        giving you what app that you want.
+        
+        informations:
+        - title
+        - developer
+        - rating
+        - donwload count
+        - lastest update
+        - lastest version
+        - file extension (APK or XAPK)
+        - icon url
+        - package extension (apk, xapk... just work in search all, by default use the file extension property)
+        - package name (com.android.example)
+        - package size (in Bytes)
+        - package version (example 1.0.2)
+        - package version code (internal code for one version in apk pure)
+        - download link 
+        - package url        
+        
+        Keyword arguments:
+        name : the query
+        Return: JSON string or Exception
+        """
+        
         self.__check_name(name)
-        top_app = self.search_top(name)
-        first_app_from_search : dict = json.loads(top_app)
-        # This variable already give us the needed information about the package
+        top_app = self.get_first_app_result(name)
+        first_app_from_search : dict = json.loads(top_app) # This variable already give us the needed information about the package
         # So dl_btn = divs.find("a", class_="download_apk_news").attrs is not necessary anymore
 
-        # The download page has more information about the package
         info_url =  str(first_app_from_search.get('package_url')) + '/download'
         html_obj = self.__soup_factory(info_url)
 
-        return json.dumps(extractors.extract_info_from_get_info(html_obj) | first_app_from_search, indent=4)
+        return json.dumps(
+            scraper.extract_info_from_get_info(html_obj) | first_app_from_search,
+            indent=4
+            )
         
     
     def download(self, name: str, version: str = None, XAPK: bool = False) -> str | None:
+        """Download an apk or xapk.
+        the downloaded app is the first result from the get_first_app_result() method, so check it before.
+        
+        Keyword arguments:
+        name : the query
+        version : the version of the app, if not specified it will be the lastest version on site
+        XAPK : specify True to download an xapk, False by default
+        Return: None
+        """
+        
         self.__check_name(name)
         
         app_type = 'XAPK' if XAPK else 'APK'
@@ -142,11 +241,16 @@ class ApkPure:
         return self.__downloader(url=base_url, name=name, version_code=version_code)
 
     def __downloader(self, url: str, name : str = None, version_code : str = None) -> str | None:
+        """The downlaoder method, dont use separetaly.
+        """
+        
         response = self.__get_response(url=url, stream=True)
         file_size = int(response.headers.get('Content-Length', 0))
         
-        filename = response.headers.get('Content-Disposition').split('filename=')[1].replace('"', '')
-        with open(f'{version_code}_{filename}', 'wb') as package_file:
+        # handle the name of filename
+        filename = response.headers.get('Content-Disposition').split('filename=')[1].replace('"', '').replace(" ", '').replace(';', '').replace(':', '')
+        
+        with open(f'{filename}', 'wb') as package_file:
             progress_bar = tqdm(total=file_size, unit='B', unit_scale=True, desc=f'Downloading {name}',dynamic_ncols=True, leave=True)
             for chunk in response.iter_content(chunk_size=1024):
                 if chunk:
